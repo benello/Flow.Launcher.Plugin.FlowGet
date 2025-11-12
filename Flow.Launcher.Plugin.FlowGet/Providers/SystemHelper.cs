60db0d497ece512bfd74ee64c96066af0d39dc62
benello <behnjaminbonello@fastmail.com>
2025-11-12 21:55:54 +0100

    Add admin privilege checks and restructure code into Providers namespace.

---
+++ Flow.Launcher.Plugin.FlowGet/Providers/SystemHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace Flow.Launcher.Plugin.FlowGet.Providers;
+
+internal static class SystemHelper
+{
+	public static bool CheckAdministratorPrivileges()
+	{
+		if (Environment.OSVersion.Platform == PlatformID.Unix)
+			return false;
+
+		using (var current = WindowsIdentity.GetCurrent(false))
+			return current != null && new WindowsPrincipal(current).IsInRole(WindowsBuiltInRole.Administrator);
+	}
+}
